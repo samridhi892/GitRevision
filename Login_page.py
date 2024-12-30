4903bed922a8069d55aa232cc45080da77eea015
Samridhi892 <samridhi.pradhan@gmail.com>
2024-12-30 19:42:55 +0530

    Changes in Login page by branch-X

--- Login_page.py
+++ Login_page.py
@@ -1,6 +1,11 @@
+import time
+
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
 driver.get("https://www.bigbasket.com/")
 driver.maximize_window()
 
+driver.find_element(By.CLASS_NAME, "Button-sc-1dr2sn8-0").click()
+time.sleep(2)
