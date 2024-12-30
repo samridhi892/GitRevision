3f980c0035b636bec01a451a719f1e6de9f1af77
Samridhi829 <samridhipradhan@gmail.com>
2024-12-30 19:03:34 +0530

    Commit from X for changes in uniques_values

--- unique_values.py
+++ unique_values.py
@@ -1,9 +1,22 @@
 
 list_1 = [1,2,3,3,3,4,4,4,5,5,6]
 new_list = []
-
+ct = 0
+d = {}
 for i in set(list_1):
     print(f"{i} occurs {list_1.count(i)} times")
+
+#Another method
+for num in list_1:
+    if num in d:
+        d[num]+=1
+    else:
+        d[num]=1
+
+print(d)
+for k,v in d.items():
+    print(f"{k} occurs {v} times")
+
 
 
 
